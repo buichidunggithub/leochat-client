bc2a1e6d6e543e4deb1251c02dbec310cb297d85
Chí Dũng Bùi <chidungbui@Chis-MacBook-Pro.local>
2022-09-27 10:41:35 +0700

    change package

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,46 +1,46 @@
-import React, { useContext, useState, useEffect, createContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { auth } from '../firebase';
-import {
-    GoogleAuthProvider,
-    signInWithPopup,
-    signInWithRedirect,
-    signOut,
-    onAuthStateChanged
-} from 'firebase/auth';
+// import React, { useContext, useState, useEffect, createContext } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { auth } from '../firebase';
+// import {
+//     GoogleAuthProvider,
+//     signInWithPopup,
+//     signInWithRedirect,
+//     signOut,
+//     onAuthStateChanged
+// } from 'firebase/auth';
 
-const AuthContext = createContext();
+// const AuthContext = createContext();
 
-export const AuthContextProvider = ({ children }) => {
-    const [user, setUser] = useState({});
+// export const AuthContextProvider = ({ children }) => {
+//     const [user, setUser] = useState({});
 
-    const googleSignIn = () => {
-        const provider = new GoogleAuthProvider();
-        signInWithPopup(auth, provider).then((result) => {
-            return result;
-        });
-    }
+//     const googleSignIn = () => {
+//         const provider = new GoogleAuthProvider();
+//         signInWithPopup(auth, provider).then((result) => {
+//             return result;
+//         });
+//     }
 
-    const logOut = () => {
-        signOut(auth);
-    }
+//     const logOut = () => {
+//         signOut(auth);
+//     }
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser);
-        });
-        return () => {
-            unsubscribe();
-        };
-    }, []);
+//     useEffect(() => {
+//         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+//             setUser(currentUser);
+//         });
+//         return () => {
+//             unsubscribe();
+//         };
+//     }, []);
 
-    return (
-        <AuthContext.Provider value={{ googleSignIn, logOut, user }}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+//     return (
+//         <AuthContext.Provider value={{ googleSignIn, logOut, user }}>
+//             {children}
+//         </AuthContext.Provider>
+//     )
+// }
 
-export const UserAuth = () => {
-    return useContext(AuthContext);
-}
+// export const UserAuth = () => {
+//     return useContext(AuthContext);
+// }
